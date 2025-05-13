d9139e336be292f86a32d089c00b31a4a3616674
Mathew Vieira <mvieirabezerra@gmail.com>
2025-05-13 14:28:57 -0300

    build: standardization

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default [
   { ignores: ['dist'] },
@@ -13,21 +14,19 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
   },
+  eslintConfigPrettier
 ]
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Card.module.css'
+
+export default function Card() {
+  return (
+    <div className={styles.card}>
+      <h1>Título</h1>
+    </div>
+  )
+}
