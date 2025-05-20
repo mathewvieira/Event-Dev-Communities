9aaaa95d91be6d43e779fc0cf6aa36ec747ced51
Ester Farias <ester.fariasbr@hotmail.com>
2025-05-20 11:28:45 -0300

    feat: implementation of the shared folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,46 @@ import Container from '@mui/material/Container'
 import Navbar from '@/shared/components/Navbar'
 
 import { FeaturedCardGroup } from '@/shared/components/FeaturedCard'
+import EventCard from './components/Card'
+import { Grid } from '@mui/material/Grid'
 
 export default function App() {
   return (
-    <Container>
-      <Navbar />
-      <FeaturedCardGroup />
-    </Container>
+    <>
+      <Container>
+        <Navbar />
+        <Grid
+          container
+          spacing={4}
+          justifyContent='center'
+        >
+          <Grid item>
+            <EventCard
+              image='/reactconf.jpg'
+              title='React Conf Nordeste 2025'
+              orgName='Frontend Ceará'
+              orgLogo='https://cdn-icons-png.flaticon.com/512/684/684908.png'
+              description='A maior conferência de React da América Latina. Palestras, workshops e muito networking'
+              date='16/05/2025'
+              location='Centro de Eventos - Avenida Washington Soares'
+              link='https://www.ingressos.com'
+            />
+          </Grid>
+          <Grid item>
+            <EventCard
+              image='/pythonnordeste.jpg'
+              title='Python Nordeste 2025'
+              orgName='Python Nordeste'
+              orgLogo='https://cdn-icons-png.flaticon.com/512/919/919852.png'
+              description='Promover o aprendizado, o compartilhamento de conhecimento e o fortalecimento de laços entre os participantes.'
+              date='20/06/2025 à 22/06/2025'
+              location='Teresina - PI'
+              link='https://www.even3.com.br/python-nordeste-2025/'
+            />
+          </Grid>
+        </Grid>
+        <FeaturedCardGroup />
+      </Container>
+    </>
   )
 }
--- src/shared/components/Card/index.jsx
+++ src/shared/components/Card/index.jsx
@@ -1,9 +1,80 @@
-import styles from './Card.module.css'
+import { Card, CardMedia, CardContent, Typography, Avatar, Box, Link, Stack } from '@mui/material'
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
+import PlaceIcon from '@mui/icons-material/Place'
+import LinkIcon from '@mui/icons-material/Link'
 
-export default function Card() {
+function EventCard({ image, title, orgName, orgLogo, description, date, location, link }) {
   return (
-    <div className={styles.card}>
-      <h1>Título</h1>
-    </div>
+    <Card sx={{ width: 340, borderRadius: 3, boxShadow: 3 }}>
+      <CardMedia
+        component='img'
+        height='180'
+        image={image}
+        alt={title}
+      />
+      <CardContent>
+        <Typography
+          variant='h6'
+          gutterBottom>
+          {title}
+        </Typography>
+
+        <Box
+          display='flex'
+          alignItems='center'
+          gap={1}
+          mb={1}>
+          <Avatar
+            alt={orgName}
+            src={orgLogo}
+            sx={{ width: 28, height: 28 }}
+          />
+          <Typography
+            variant='body2'
+            color='text.secondary'>
+            {orgName}
+          </Typography>
+        </Box>
+
+        <Typography
+          variant='body2'
+          color='text.secondary'
+          mb={2}>
+          {description}
+        </Typography>
+
+        <Stack spacing={1}>
+          <Box
+            display='flex'
+            alignItems='center'
+            gap={1}>
+            <CalendarTodayIcon fontSize='small' />
+            <Typography variant='body2'>{date}</Typography>
+          </Box>
+          <Box
+            display='flex'
+            alignItems='center'
+            gap={1}>
+            <PlaceIcon fontSize='small' />
+            <Typography variant='body2'>{location}</Typography>
+          </Box>
+          <Box
+            display='flex'
+            alignItems='center'
+            gap={1}>
+            <LinkIcon fontSize='small' />
+            <Link
+              href={link}
+              target='_blank'
+              rel='noopener'
+              underline='hover'>
+              {link}
+            </Link>
+          </Box>
+        </Stack>
+      </CardContent>
+    </Card>
   )
 }
+
+export default EventCard
