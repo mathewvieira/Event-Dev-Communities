0287a9458b6815e8dad3b2c911a4cd97ccd24b8f
esterfar18 <ester.fariasbr@hotmail.com>
2025-05-23 14:06:09 -0300

    feat: creation of the search bar structure and import of mui components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Container from '@mui/material/Container'
 import Navbar from '@/shared/components/Navbar'
 
 import { FeaturedCardGroup } from '@/shared/components/FeaturedCard'
+import SearchBar from './shared/components/Searchbar'
+import SearchBarGroup from './shared/components/Searchbar'
 
 export default function App() {
   return (
     <Container>
       <Navbar />
       <FeaturedCardGroup />
+      <SearchBarGroup />
     </Container>
   )
 }
---
+++ src/shared/components/Searchbar/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import SearchIcon from '@mui/icons-material/Search'
+import GridViewIcon from '@mui/icons-material/GridView'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+
+export default function SearchBarGroup() {
+  const [filter, setFilter] = React.useState('Mais próximos')
+
+  return (
+    <Box
+      display='flex'
+      gap={1}
+      alignItems='center'
+      sx={{ backgroundColor: '#f9fafb', padding: 2 }}
+    >
+      {/* Campo de busca */}
+      <TextField
+        variant='outlined'
+        placeholder='Buscar eventos...'
+        size='small'
+        sx={{ flex: 1 }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position='start'>
+              <SearchIcon />
+            </InputAdornment>
+          )
+        }}
+      />
+
+      {/* Filtro dropdown */}
+      <Select
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        size='small'
+        sx={{ minWidth: 150 }}
+      >
+        <MenuItem value='Mais próximos'>Mais próximos</MenuItem>
+        <MenuItem value='Mais recentes'>Mais recentes</MenuItem>
+        <MenuItem value='Mais populares'>Mais populares</MenuItem>
+      </Select>
+
+      {/* Botões de visualização */}
+      <IconButton>
+        <GridViewIcon />
+      </IconButton>
+
+      <IconButton sx={{ 'backgroundColor': '#ff6b00', 'color': 'white', '&:hover': { backgroundColor: '#e65c00' } }}>
+        <CalendarMonthIcon />
+      </IconButton>
+    </Box>
+  )
+}
