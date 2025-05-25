6342db97224dc70c93efd803abe6f8a3ebc870df
laisfrr <laisf.oliv@gmail.com>
2025-05-24 22:10:52 -0300

    feat: create footer component and configure routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import Container from '@mui/material/Container'
-import Navbar from '@/shared/components/Navbar'
-import { FeaturedCardGroup } from '@/shared/components/FeaturedCard'
-import SectionHeader from './shared/components/SectionHeader'
-import CallToAction from './shared/components/CallToAction'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <>
-      <Container>
-        <Navbar />
-        <SectionHeader
-          title='Comunidades em Destaque'
-          subtitle='Conheça as comunidades dev mais ativas da plataforma'
-          link='#'
-          linkText='Ver todas as Comunidades'
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
         />
-        <FeaturedCardGroup />
-      </Container>
-
-      <CallToAction />
-    </>
+        {/* <Route
+          path='/eventos'
+          element={<Events />}
+        />
+        <Route
+          path='/comunidades'
+          element={<Communities />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Container from '@mui/material/Container'
+import Navbar from '@/shared/components/Navbar'
+import { FeaturedCardGroup } from '@/shared/components/FeaturedCard'
+import SectionHeader from '@/shared/components/SectionHeader'
+import CallToAction from '@/shared/components/CallToAction'
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <Navbar />
+        <SectionHeader
+          title='Comunidades em Destaque'
+          subtitle='Conheça as comunidades dev mais ativas da plataforma'
+          link='#'
+          linkText='Ver todas as Comunidades'
+        />
+        <FeaturedCardGroup />
+      </Container>
+      <CallToAction />
+    </>
+  )
+}
