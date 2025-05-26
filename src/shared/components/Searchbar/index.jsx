ccb257878faec21ae66658f1c57b871ca3accfa9
esterfar18 <ester.fariasbr@hotmail.com>
2025-05-26 09:39:31 -0300

    feat: estruturação da paginas eventos

--- src/shared/components/Searchbar/index.jsx
+++ src/shared/components/Searchbar/index.jsx
@@ -11,113 +11,116 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import AppsIcon from '@mui/icons-material/Apps'
 import EventIcon from '@mui/icons-material/Event'
 import SearchIcon from '@mui/icons-material/Search'
+import Container from '@mui/material/Container'
 
 export default function Searchbar() {
   const [view, setView] = useState('calendar')
   const [filter, setFilter] = useState('nearby')
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        mb: '2rem',
-        justifyContent: 'center',
-        backgroundColor: '#f9fafb'
-      }}
-    >
+    <Container>
       <Box
         sx={{
           display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          maxWidth: 1200,
-          gap: 2
+          mb: '2rem',
+          justifyContent: 'center',
+          backgroundColor: '#f9fafb'
         }}
       >
-        <TextField
-          placeholder='Buscar eventos...'
-          variant='outlined'
-          fullWidth
+        <Box
           sx={{
-            '&': {
-              height: 44,
-              minWidth: 150,
-              borderRadius: 2,
-              backgroundColor: 'white',
-              boxShadow: '0 0 0 1px #e0e0e0'
-            },
-            '& .MuiOutlinedInput-notchedOutline': {
-              border: 'none'
-            },
-            '& .MuiInputBase-root': {
-              height: '44px'
-            }
-          }}
-          slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <SearchIcon color='action' />
-                </InputAdornment>
-              )
-            }
-          }}
-        />
-
-        <Select
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          sx={{
-            '&': {
-              backgroundColor: 'white',
-              borderRadius: 2,
-              boxShadow: '0 0 0 1px #e0e0e0',
-              height: 44,
-              minWidth: 190
-            },
-            '& fieldset': {
-              border: 'none'
-            }
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+            maxWidth: 1200,
+            gap: 2
           }}
         >
-          <MenuItem value='nearby'>Mais próximos</MenuItem>
-          <MenuItem value='recent'>Mais recentes</MenuItem>
-          <MenuItem value='popular'>Populares</MenuItem>
-        </Select>
+          <TextField
+            placeholder='Buscar eventos...'
+            variant='outlined'
+            fullWidth
+            sx={{
+              '&': {
+                height: 44,
+                Width: '100%',
+                borderRadius: 2,
+                backgroundColor: 'white',
+                boxShadow: '0 0 0 1px #e0e0e0'
+              },
+              '& .MuiOutlinedInput-notchedOutline': {
+                border: 'none'
+              },
+              '& .MuiInputBase-root': {
+                height: '44px'
+              }
+            }}
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon color='action' />
+                  </InputAdornment>
+                )
+              }
+            }}
+          />
 
-        <ToggleButtonGroup
-          value={view}
-          exclusive
-          onChange={(_, val) => {
-            if (val) setView(val)
-          }}
-        >
-          <ToggleButton
-            value='grid'
-            style={{ backgroundColor: view === 'grid' ? '#FC692D' : '' }}
+          <Select
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
             sx={{
-              border: 'none',
-              borderRadius: '10px',
-              minWidth: 44,
-              height: 44
+              '&': {
+                backgroundColor: 'white',
+                borderRadius: 2,
+                boxShadow: '0 0 0 1px #e0e0e0',
+                height: 44,
+                minWidth: 190
+              },
+              '& fieldset': {
+                border: 'none'
+              }
             }}
           >
-            <AppsIcon />
-          </ToggleButton>
-          <ToggleButton
-            value='calendar'
-            style={{ backgroundColor: view === 'calendar' ? '#FC692D' : '' }}
-            sx={{
-              border: 'none',
-              borderRadius: '10px',
-              minWidth: 44,
-              height: 44
+            <MenuItem value='nearby'>Mais próximos</MenuItem>
+            <MenuItem value='recent'>Mais recentes</MenuItem>
+            <MenuItem value='popular'>Populares</MenuItem>
+          </Select>
+
+          <ToggleButtonGroup
+            value={view}
+            exclusive
+            onChange={(_, val) => {
+              if (val) setView(val)
             }}
           >
-            <EventIcon />
-          </ToggleButton>
-        </ToggleButtonGroup>
+            <ToggleButton
+              value='grid'
+              style={{ backgroundColor: view === 'grid' ? '#FC692D' : '' }}
+              sx={{
+                border: 'none',
+                borderRadius: '10px',
+                minWidth: 44,
+                height: 44
+              }}
+            >
+              <AppsIcon />
+            </ToggleButton>
+            <ToggleButton
+              value='calendar'
+              style={{ backgroundColor: view === 'calendar' ? '#FC692D' : '' }}
+              sx={{
+                border: 'none',
+                borderRadius: '10px',
+                minWidth: 44,
+                height: 44
+              }}
+            >
+              <EventIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   )
 }
