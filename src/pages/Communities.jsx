f7d3136b75bc908f0e240b7278fd16da3b1bc908
esterfar18 <ester.fariasbr@hotmail.com>
2025-05-27 13:14:38 -0300

    feat: importações feitas

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -1,12 +1,30 @@
+import FeaturedCardGroup from '@/shared/components/FeaturedCard/FeaturedCardGroup'
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Searchbar from '@/shared/components/Searchbar'
 
 export default function Communities() {
   return (
-    <Container
-      maxWidth='xl'
-      sx={{ paddingTop: '4.5rem' }}
-    >
-      <h1>Comunidades</h1>
-    </Container>
+    <Box>
+      <Container
+        maxWidth='xl'
+        sx={{ paddingTop: '4.5rem' }}
+      >
+        <Typography maxWidth='xl'>
+          <main className='px-6 py-10 max-w-7xl mx-auto'>
+            <h1 className='text-3xl font-semibold mb-2'>Comunidades </h1>
+            <p className='text-gray-500 mb-6'>Encontre comunidades de desenvolvedores para participar de eventos.</p>
+          </main>
+        </Typography>
+
+        <Searchbar />
+
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <FeaturedCardGroup />
+          <FeaturedCardGroup />
+        </Box>
+      </Container>
+    </Box>
   )
 }
