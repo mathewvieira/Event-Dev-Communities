b25a0865230c98eafb89c7d37c622c726763b881
Diego Andrade <Diegoandradeoliveira33@gmail.com>
2025-06-09 10:22:56 -0300

    feat(comunidades): atualização da página de comunidades

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -1,12 +1,14 @@
 import Container from '@mui/material/Container'
+import FeaturedCardGroup from '@/shared/components/FeaturedCard/FeaturedCardGroup'
 
 export default function Communities() {
   return (
     <Container
       maxWidth='xl'
-      sx={{ paddingTop: '4.5rem' }}
-    >
-      <h1>Comunidades</h1>
+      sx={{ paddingTop: '4.5rem', paddingBottom: '2rem' }}>
+      <h1 style={{ marginBottom: '2rem' }}>Comunidades</h1>
+
+      <FeaturedCardGroup />
     </Container>
   )
 }
